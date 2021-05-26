4eb70a9b318f8fe8bfc3c0f5bfb04f9bd9184b8f
itzyaieeshu <62168657+itzyaieeshu@users.noreply.github.com>
2021-05-26 18:03:15 +1000

    routes update

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var data = require('./data')
 
 const port = 3000
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
     res.send('Welcome to our schedule website');
 })
@@ -19,6 +22,10 @@ app.get('/users/:id', (req, res) => {
 })
 app.get('/users/:id/schedules', (req, res) => {
     res.send(data.schedules)
+})
+app.post('/users', (req, res) => {
+    data.users.push(req.body)
+    res.send(data.users)
 })
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
