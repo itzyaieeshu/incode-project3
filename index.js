86e949de6a5a7c235dd8024bc3af6c5b1678cd63
itzyaieeshu <62168657+itzyaieeshu@users.noreply.github.com>
2021-05-28 09:57:29 +1000

    schedule fix

--- index.js
+++ index.js
@@ -1,32 +1,59 @@
-const express = require('express')
+const express = require('express');
+const bcrypt = require('bcrypt');
 
-const app = express()
-var data = require('./data')
+const app = express();
+const data = require('./data');
 
-const port = 3000
+const saltRounds = 10;
+const port = 3000;
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Welcome to our schedule website');
-})
+  res.send('Welcome to our schedule website');
+});
 app.get('/users', (req, res) => {
-    res.send(data.users);
-})
+  res.send(data.users);
+});
 app.get('/schedules', (req, res) => {
-    res.send(data.schedules);
-})
+  res.send(data.schedules);
+});
 app.get('/users/:id', (req, res) => {
-    res.send(data.users[req.params.id])
-})
+  res.send(data.users[req.params.id]);
+});
 app.get('/users/:id/schedules', (req, res) => {
-    res.send(data.schedules)
-})
+  const id = Number(req.params.id);
+  const result = [];
+  for (let i = 0; i < data.schedules.length; i++) {
+    const schedule = data.schedules[i];
+    console.log(schedule);
+    console.log(schedule.user_id);
+    const userId = Number(schedule.user_id);
+    console.log(userId);
+    if (userId === id) {
+      result.push(schedule);
+    }
+  }
+  res.send(result);
+});
 app.post('/users', (req, res) => {
-    data.users.push(req.body)
-    res.send(data.users)
-})
+  const arr = req.body;
+  let hashedPassword;
+  bcrypt.hash(arr.password, saltRounds, (err, hash) => {
+    if (err) {
+      console.log(err);
+    } else {
+      hashedPassword = hash;
+      console.log(hashedPassword)
+      arr.password = hashedPassword;
+    }
+  });
+  console.log(arr.password);
+  // console.log(typeof arr);
+  data.users.push(arr);
+  res.send(data.users);
+});
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-})
+  console.log(`Mr.coffee app listening at http://localhost:${port}`);
+});
