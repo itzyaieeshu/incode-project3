6a4495d7551139fece59c989524b9b9be61b8a9b
itzyaieeshu <62168657+itzyaieeshu@users.noreply.github.com>
2021-06-09 12:38:03 +1000

    select,instert for user & schedules

--- index.js
+++ index.js
@@ -26,10 +26,16 @@ app.get('/', (req, res) => {
   });
 });
 app.get('/users', (req, res) => {
-  res.render('pages/users', {
-    documentTitle: 'Users',
-    pageName: 'users',
-    users: data.users,
+  db.any('SELECT * FROM users')
+  .then((users) => {
+    res.render('pages/users', {
+      documentTitle: 'Users',
+      pageName: 'users',
+      users,
+    });
+  })
+  .catch((err) => {
+    res.send(err);
   });
 });
 app.get('/user/new', (req, res) => {
@@ -39,20 +45,33 @@ app.get('/user/new', (req, res) => {
   });
 });
 app.get('/schedules', (req, res) => {
-  res.render('pages/schedules', {
-    documentTitle: 'Schedules',
-    pageName: 'schedules',
-    schedules: data.schedules,
+  db.any('SELECT * FROM schedules')
+  .then((schedules) => {
+    res.render('pages/schedules', {
+      documentTitle: 'Schedules',
+      pageName: 'schedules',
+      schedules,
+    });
+  })
+  .catch((err) => {
+    res.send(err);
   });
 });
 app.get('/schedule/new', (req, res) => {
-  res.render('pages/add-schedule', {
-    documentTitle: 'Add Schedules',
-    pageName: '',
-    users: data.users,
-    days: day,
-    timeOption: time,
+  db.any('SELECT * FROM users')
+  .then((users) => {
+    res.render('pages/add-schedule', {
+      documentTitle: 'Add Schedules',
+      pageName: '',
+      days: day,
+      timeOption: time,
+      users,
+    });
+  })
+  .catch((err) => {
+    res.send(err);
   });
+  
 });
 app.get('/users/:id', (req, res) => {
   res.send(data.users[req.params.id]);
@@ -69,14 +88,30 @@ app.get('/users/:id/schedules', (req, res) => {
   res.send(result);
 });
 app.post('/schedule/add', (req, res) => {
-  const arr = {
+  const time = (str) => {
+    let result;
+    if (str.includes('AM')) {
+      const resultTime = str.match(/(\d+)/);
+      result = `${resultTime[0]}:00:00`;
+    } else if (str.includes('PM')) {
+      const resultTime = str.match(/(\d+)/);
+      result = `${Number(resultTime[0]) + 12}:00:00`;
+    }
+    return result;
+  };
+  const schedule = {
     user_id: Number(req.body.user_id),
     day: Number(req.body.day),
-    start_at: req.body.start_at,
-    end_at: req.body.end_at,
+    start_at: time(req.body.start_at),
+    end_at: time(req.body.end_at),
 };
-  data.schedules.push(arr);
-  res.redirect('/schedules');
+  db.any('INSERT INTO schedules(user_id, day, start_at, end_at) Values ($1,$2,$3,$4);', [schedule.user_id, schedule.day, schedule.start_at, schedule.end_at])
+  .then(() => {
+    res.redirect('/schedules');
+  })
+  .catch((err) => {
+    res.send(err);
+  });
 });
 app.post('/user/add', (req, res) => {
   const user = req.body;
@@ -90,7 +125,13 @@ app.post('/user/add', (req, res) => {
   // });
   user.password = SHA256(user.password).toString();
   data.users.push(user);
-  res.redirect('/users');
+  db.any('INSERT INTO users(firstname, lastname, email, password) Values ($1,$2,$3,$4);', [user.firstname, user.lastname, user.email, user.password])
+  .then(() => {
+    res.redirect('/users');
+  })
+  .catch((err) => {
+    res.send(err);
+  });
 });
 app.listen(port, () => {
   console.log(`Mr.coffee app listening at http://localhost:${port}`);
